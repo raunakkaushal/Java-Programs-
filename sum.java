bab848a948b37b5284aa03362355a4feef573d6f
Raunak Kaushal <grsraunak@gmail.com>
2024-07-02 09:26:32 +0530

    add function\method approach

--- sum.java
+++ sum.java
@@ -1,8 +1,13 @@
 public class sum{
+    public static int calculateSum(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
     public static void main(String args[]){
         int a = 10;
         int b = 5; 
 
-        System.out.println("Sum is : "+ (a + b));
+        int sum = calculateSum(a, b);
+        System.out.println("Sum is : " + sum);
     }
 }
